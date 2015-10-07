48c6e143d6711eef3d1183b60c97f458ee5cb0c6
Amit Shah <ashah325i@gmail.com>
2015-10-06 22:54:08 -0400

    continued work in main.js

--- main.js
+++ main.js
@@ -2,23 +2,20 @@
 
   $('.box1').on('click', 'h2', function(event){
     var btn = $(event.target);
-    // add a class that has a larger height to box 1
-    //add a class to box 2 & 3 that has a height of 27 px
-    
+    btn.addClass('h2.box2');  
   });
 
   $('.box2').on('click', 'h2', function(event){
     var btn = $(event.target);
-    btn.addClass('box2');
+    btn.addClass('h2.box3');
   });
 
   $('.box3').on('click', 'h2', function(event){
     var btn = $(event.target);
-    btn.addClass('box3');
+    btn.addClass('h2.box1');
   });
 
-
-
-
+// add a class that has a larger height to box 1
+//add a class to box 2 & 3 that has a height of 27 px
 
 }());
